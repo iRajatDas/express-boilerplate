d6faae578041a578f05e277aa23d0766e0d12061
Rajat Das <rajat@Rajats-MacBook-Air.local>
2024-06-01 12:16:29 +0530

    ui: bun package manager; js cleanup

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var env_1 = require("./config/env");
-var port = env_1.env.PORT || 3000;
-app_1.default.listen(port, function () {
-    console.log("Server running on port ".concat(port));
-});
